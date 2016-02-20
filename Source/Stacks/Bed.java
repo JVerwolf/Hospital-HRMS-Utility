f24231d0ab549ee2ce3d84eee151c49c8c836d4d
John Verwolf <User@User-PC>
2016-02-20 13:48:20 -0800

    Added Bed

--- Source/Stacks/Bed.java
+++ Source/Stacks/Bed.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Stacks;
 
 /**
  *
- * @author C0432660
+ * @author John Verwolf
  */
 public class Bed {
+
+    private String location;
+    private String name;
+
+    /**
+     * Initializes this Bed object's name and location to a specified name and
+     * location
+     *
+     * @param name     The specified name for the bed
+     * @param location the specified location where this bed is located
+     */
+    public Bed(String name, String location) {
+        this.location = location;
+        this.name = name;
+    }
     
+//    /**
+//     * Static factory method design pattern
+//     * @param name
+//     * @param location
+//     * @return 
+//     */
+//    public static Bed getInstBedNameLocation(String name, String location){
+//        return new Bed(name, location);
+//    }
+    
+    /**
+     * @return this bed object's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set this bed object's name
+     *
+     * @param name bed object's name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return a String representation of this Bed object with the name followed
+     *         by a space followed by location
+     */
+    @Override
+    public String toString() {
+        return "Bed{" + "location=" + location + ", name=" + name + '}';
+    }
+
 }
