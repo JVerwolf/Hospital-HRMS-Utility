d53ee310439d68bdbb847a403aa448e1d575ad1e
John Verwolf <User@User-PC>
2016-02-20 13:51:26 -0800

    Added Main

---
+++ Source/Stacks/StacksMainClass.java
@@ -0,0 +1,19 @@
+package Stacks;
+
+/**
+ *
+ * @author John Verwolf
+ */
+public class StacksMainClass {
+    
+    public StacksMainClass(){
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
