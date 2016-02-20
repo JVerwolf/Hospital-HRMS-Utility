a16516103412510245c85140ad6cebf4d437a450
John Verwolf <User@User-PC>
2016-02-20 13:29:38 -0800

    Added Bed, CareFacility, and Patient classes.

--- Source/Stacks/CareFacility.java
+++ Source/Stacks/CareFacility.java
@@ -1,22 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Stacks;
 
 import ADT.Company;
+import ADT.HourlyEmployee;
+
 /**
  *
- * @author C0432660
+ * @author John Verwolf
  */
 public class CareFacility extends Company {
 
+    private java.lang.String facilityName;
+    private ArrayStack<Patient> patientStack;
+    private ArrayStack<Bed> bedStack;
+    private HourlyEmployee[] hourlyEmployee;
+
     /**
-     * @param args the command line arguments
+     * Initializes this Care Facility object's name and array of employees,
+     * array of beds and array of patients to null
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public CareFacility() {
+        facilityName = null;
+        hourlyEmployee = null;
+        bedStack = null;
+        patientStack = null;
+    }
+
+    /**
+     * Initializes this Care Facility object's name and array of employees array
+     * of beds and array of patients from a specified name and array of
+     * employees, array of beds and array of patients
+     *
+     * @param facilityName
+     * @param patientStack
+     * @param bedStack
+     * @param hourlyEmployee
+     */
+    public CareFacility(String facilityName, ArrayStack<Patient> patientStack, ArrayStack<Bed> bedStack, HourlyEmployee[] hourlyEmployee) {
+        this.facilityName = facilityName;
+        this.patientStack = patientStack;
+        this.bedStack = bedStack;
+        this.hourlyEmployee = hourlyEmployee;
+    }
+
+    /**
+     * Initializes this Care Facility object's name and array of employees from
+     * specified name and array of employees The array of beds and array of
+     * patients will be set to null
+     *
+     * @param facilityName
+     * @param hourlyEmployee
+     */
+    public CareFacility(String facilityName, HourlyEmployee[] hourlyEmployee) {
+        this.facilityName = facilityName;
+        this.hourlyEmployee = hourlyEmployee;
+        bedStack = null;
+        patientStack = null;
     }
     
+    /**
+     * Assign registered patients to existing beds
+     */
+    public void assignBed(){
+        throw new UnsupportedOperationException();//TODO:code method
+    }
+    
+
 }
